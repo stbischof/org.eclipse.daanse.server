c6f1ff33fa129f9c30d9a81b4581341d37976448
Stefan Bischof <stbischof@bipolis.org>
2026-02-02 15:53:51 +0100

    improve log
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

--- application/probe/start.bat
+++ application/probe/start.bat
@@ -11,18 +11,18 @@ set "LOGBACK_CONFIG=.\logback.xml"
 REM Check if java -version works
 "%JAVA_EXEC%" -version >nul 2>&1
 if errorlevel 1 (
-    echo Java is installed but not working properly (java -version failed).
+    echo "Java is installed but not working properly (java -version failed)."
     exit /b 1
 )
 
 REM Check Logback configuration file exists
 if not exist "%LOGBACK_CONFIG%" (
-    echo Logback configuration file "%LOGBACK_CONFIG%" not found.
+    echo "Logback configuration file "%LOGBACK_CONFIG%" not found."
     exit /b 1
 )
 
 REM Start Server
-echo Starting server...
+echo "Starting server..."
 "%JAVA_EXEC%" -Dlogback.configurationFile=file:"%LOGBACK_CONFIG%" -jar daanse.probe.jar
 
 endlocal
